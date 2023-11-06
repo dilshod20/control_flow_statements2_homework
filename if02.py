68297b5cf595af72ae7884dd03a5f1edee159c5f
dilshod20 <dilshodraxmatov277@gmail.com>
2023-11-06 11:40:53 +0500

    2

--- if02.py
+++ if02.py
@@ -8,4 +8,15 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a < b:
+        if a < c:
+            return a
+        else:
+            return b 
+    else:
+        if b > a:
+            return b
+        else:
+            return a
+print(main(1,4,2)) 
+print(main(-5,-3,-1))  
