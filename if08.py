851b6bdeff5c063858c7d8725e626fc6d95c128e
dilshod20 <dilshodraxmatov277@gmail.com>
2023-11-07 21:59:03 +0500

    8

--- if08.py
+++ if08.py
@@ -14,4 +14,19 @@ def main(number):
     Returns:
         str: return answer.
     """
-    return
+    if number < 0:
+        return "Monday"
+    elif number >= 1 and number <=2:
+        return "Tuesday"
+    elif number >= 2 and number <=3:
+        return "Wednesday"
+    elif number >= 3 and number <=4:
+        return "Thursday"
+    elif number >= 4 and number <=5:
+        return "Friday"
+    elif number >= 5 and number <=6:
+        return "Saturday"
+    elif number >= 6 and number <=7:
+        return "Sunday"
+print(main(2))
+print(main(6))
