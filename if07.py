589133eb525c8765c5736fa12fdff91b835d676a
dilshod20 <dilshodraxmatov277@gmail.com>
2023-11-07 21:39:33 +0500

    7

--- if07.py
+++ if07.py
@@ -13,4 +13,19 @@ def main(temp):
     Returns:
         str: return answer.
     """
-    return
+    
+    if temp < 0:
+        return "Very Cold"
+    if temp >= 1 and temp <=10:
+        return "Freezing"
+    if temp >= 11 and temp <= 20:
+        return "Cold"
+    if temp >= 21 and temp <= 30:
+        return "Normal"
+    if temp >= 31 and temp <= 40:
+        return "Hot"
+    if temp >40:
+        return "Very Hot"
+print(main(14))
+print(main(2))
+    
